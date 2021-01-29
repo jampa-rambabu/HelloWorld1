905e9706d78caca452ce9626bfdfb57614667140
jampa-rambabu <jampa.rambabu8@gmail.com>
2021-01-29 10:57:06 +0530

    Created  Hello world JAVA code

---
+++ Raambabu/src/main/java/Jampa/Raambabu/App.java
@@ -0,0 +1,13 @@
+package Jampa.Raambabu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
